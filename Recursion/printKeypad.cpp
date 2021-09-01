6746bc311f504c10808759d7ec030bdabca729a7
Pratham Vasani <73855805+JoKeR-VIKING@users.noreply.github.com>
2021-09-01 13:56:58 +0530

    Create printKeypad.cpp

---
+++ Recursion/printKeypad.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string arr[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+void helpPrint(int num, string s)
+{
+    if (num == 0)
+    {
+        cout<<s<<"\n";
+        return;
+	}
+    
+    int size = arr[num%10].length();
+    
+    for (int i=0;i<size;i++)
+    {
+        string sub = arr[num % 10][i] + s;
+        helpPrint(num / 10, sub);
+    }
+}
+
+void printKeypad(int num)
+{
+    string s;
+    
+    helpPrint(num, s);
+}
